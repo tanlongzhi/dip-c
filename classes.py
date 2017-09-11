4b586e0e5bc87f3357977ed6d3937f0f3ff718a4
tanlongzhi <tanlongzhi@gmail.com>
2017-09-11 11:18:03 -0400

    added the 3rd pass of seg (previously phase_txt.py)

--- classes.py
+++ classes.py
@@ -7,6 +7,16 @@ class Haplotypes:
 def is_known_haplotype(haplotype):
     return haplotype >= 0
 
+# rules for updating one haplotype with another (merging)
+def update_haplotype(haplotype_1, haplotype_2):
+    if haplotype_1 == haplotype_2:
+        return haplotype_1
+    elif haplotype_1 == Haplotypes.unknown:
+        return haplotype_2
+    elif haplotype_2 == Haplotypes.unknown:
+        return haplotype_1
+    return Haplotypes.conflict
+
 # a read segment (alignment)
 class Seg:
     is_read2 = False
@@ -28,6 +38,13 @@ class Seg:
         self.is_reverse = is_reverse
         self.haplotype = haplotype # if not given, default is unknown
     
+    def update_haplotype(self, is_read2, ref_name, ref_locus, haplotype):
+        if self.is_read2 == is_read2 and self.ref_name == ref_name and ref_locus - 1 >= self.ref_start and ref_locus <= self.ref_end:
+            self.haplotype = update_haplotype(self.haplotype, haplotype)
+    
+    def is_phased(self):
+        return is_known_haplotype(self.haplotype)
+    
     def to_string(self): # "m" is for mate, "." for all unknown haplotypes
         return ",".join(["m" if self.is_read2 else ".", str(self.query_start), str(self.query_end), self.ref_name, str(self.ref_start), str(self.ref_end), "-" if self.is_reverse else "+", str(self.haplotype) if is_known_haplotype(self.haplotype) else "."])
     
@@ -48,6 +65,17 @@ class Read:
     
     def num_segs(self):
         return len(self.segs)
+
+    def num_phased_segs(self):
+        num_phased_segs = 0
+        for seg in self.segs:
+            if seg.is_phased():
+                num_phased_segs += 1
+        return num_phased_segs
+            
+    def update_haplotype(self, is_read2, ref_name, ref_locus, haplotype):
+        for seg in self.segs:
+            seg.update_haplotype(is_read2, ref_name, ref_locus, haplotype)
     
     def to_string(self):
         return self.name + "\t" + "\t".join([seg.to_string() for seg in self.segs])
@@ -62,6 +90,7 @@ class SegData:
     def contains_read_name(self, name):
         return name in self.reads
 
+    # add a read; merge if exists; ignore if empty
     def add_read(self, read):
         if read.num_segs() == 0:
             return # ignore empty reads
@@ -75,10 +104,27 @@ class SegData:
         for name in self.reads.keys():
             if self.reads[name].num_segs() < 2:
                 del self.reads[name]
+                
+    # update haplotype for a specific read, if exists
+    def update_haplotype(self, name, is_read2, ref_name, ref_locus, haplotype):
+        if name in self.reads:
+            self.reads[name].update_haplotype(is_read2, ref_name, ref_locus, haplotype)
             
     def num_reads(self):
         return len(self.reads)
-            
+        
+    def num_segs(self):
+        num_segs = 0
+        for read in self.reads.values():
+            num_segs += read.num_segs()
+        return num_segs
+    
+    def num_phased_segs(self):
+        num_phased_segs = 0
+        for read in self.reads.values():
+            num_phased_segs += read.num_phased_segs()
+        return num_phased_segs
+          
     def to_string(self): # no tailing new line
         return "\n".join(read.to_string() for read in self.reads.values())
             
--- seg.py
+++ seg.py
@@ -1,14 +1,17 @@
 import sys
 import getopt
 import pysam
-from classes import Haplotypes, is_known_haplotype, Seg, Read, SegData
+import gzip
+from classes import Haplotypes, Seg, Read, SegData
 
+# add the primary alignment as a segment to a read, if satisfying conditions
 def add_primary_seg(read, bam_read, min_mapq, max_nm_per_bp):
     if bam_read.mapping_quality < min_mapq or float(bam_read.get_tag("NM")) / (bam_read.query_alignment_end - bam_read.query_alignment_start) > max_nm_per_bp:
         return
     read.add_seg(Seg(bam_read.is_read2, bam_read.query_alignment_start, bam_read.query_alignment_end, bam_read.reference_name, bam_read.reference_start, bam_read.reference_end, bam_read.is_reverse))
 
-def add_sa_segs(read, bam_read, bam_file, min_mapq, max_nm_per_bp):
+# add all SA alignments as segments to a read, if satisfying conditions
+def add_sa_segs(read, bam_read, min_mapq, max_nm_per_bp):
     sa_tags = bam_read.get_tag("SA")
     # read each part of the SA tag
     for sa_tag in sa_tags.split(";")[:-1]:
@@ -18,8 +21,7 @@ def add_sa_segs(read, bam_read, bam_file, min_mapq, max_nm_per_bp):
         ref_start = int(ref_start)
         if mapping_quality < min_mapq:
             continue
-        # hack pysam's current alignment (primary) to get SA alignment 
-        bam_read.reference_id = bam_file.get_tid(ref_name)
+        # hack pysam's current alignment (primary) to get SA alignment
         bam_read.reference_start = int(ref_start)
         bam_read.cigarstring = cigar
         if bam_read.is_reverse == (strand == "-"):
@@ -36,13 +38,16 @@ def seg(argv):
     # default parameters
     min_mapq = 20
     max_nm_per_bp = 0.05
+    min_baseq = 20
+    snp_file_name = ""
     
     # progress display parameters
     display_num_bam_reads = 1e5
+    display_num_snps = 1e4
     
     # read arguments
     try:
-        opts, args = getopt.getopt(argv[1:], "q:m:")
+        opts, args = getopt.getopt(argv[1:], "q:m:v:")
     except getopt.GetoptError as err:
         sys.stderr.write("[E::" + __name__ + "] unknown command\n")
         return 1
@@ -51,12 +56,16 @@ def seg(argv):
         sys.stderr.write("Options:\n")
         sys.stderr.write("  -q INT     min mapping quality [" + str(min_mapq) + "]\n")
         sys.stderr.write("  -m FLOAT   max edit distance per bp of aligned read [" + str(max_nm_per_bp) + "]\n")
+        sys.stderr.write("  -v         tab-delimited SNP file (format: chr, pos, paternal nucleotide, maternal nucleotide) [none]\n")
+        sys.stderr.write("  -Q         min base quality (if -v) [" + str(min_baseq) + "]\n")
         return 1
     for o, a in opts:
         if o == "-q":
             min_mapq = int(a)
         elif o == "-m":
             max_nm = int(a)
+        elif o == "-v":
+            snp_file_name = a
             
     # initialize data structure
     seg_data = SegData()
@@ -77,17 +86,18 @@ def seg(argv):
         if bam_read.has_tag("SA"):
             read = Read(bam_read.query_name)
             add_primary_seg(read, bam_read, min_mapq, max_nm_per_bp)
-            add_sa_segs(read, bam_read, bam_file, min_mapq, max_nm_per_bp)
+            add_sa_segs(read, bam_read, min_mapq, max_nm_per_bp)
         elif bam_read.is_paired and not bam_read.is_proper_pair:
             read = Read(bam_read.query_name)
             add_primary_seg(read, bam_read, min_mapq, max_nm_per_bp)
         else:
             continue
         seg_data.add_read(read)
+    sys.stderr.write("[M::" + __name__ + "] pass 1 done: read " + str(num_bam_reads) + " alignments; added " + str(seg_data.num_reads()) + " candidate reads (" + str(round(100.0 * seg_data.num_reads() / num_bam_reads, 2)) + "% of alignments)\n")
 
-    # pass 2: find mates of all reads from pass 1
+    # pass 2: find mates of all reads from pass 1, and then removes reads with only one segment
     num_bam_reads = 0
-    bam_file.reset()
+    bam_file.reset() # not a documented API, but works
     for bam_read in bam_file.fetch(until_eof=True):
         num_bam_reads += 1
         if num_bam_reads % display_num_bam_reads == 0:
@@ -100,14 +110,46 @@ def seg(argv):
         else:
             continue
         seg_data.add_read(read)
-            
-    sys.stderr.write("[M::" + __name__ + "] cleaning " + str(seg_data.num_reads()) + " candidate reads\n")
+    sys.stderr.write("[M::" + __name__ + "] pass 2: cleaning " + str(seg_data.num_reads()) + " candidate reads\n")
     seg_data.clean()
-    sys.stderr.write("[M::" + __name__ + "] writing output for " + str(seg_data.num_reads()) + " candidate reads \n")
-    sys.stdout.write(seg_data.to_string()+"\n")
+    sys.stderr.write("[M::" + __name__ + "] pass 2 done: read " + str(num_bam_reads) + " alignments; kept " + str(seg_data.num_reads()) + " candidate reads (" + str(round(100.0 * seg_data.num_reads() / num_bam_reads, 2)) + "% of alignments)\n")
     
-    # pass 3: find haplotype information
-        
-    return 0
-
+    # pass 3: (if -v) find haplotype information based on the SNP file
+    if snp_file_name != "":
+        num_snps = 0
+        snp_file = gzip.open(snp_file_name, "rb") if snp_file_name.endswith(".gz") else open(snp_file_name, "rb")
+        for snp_file_line in snp_file:
+            num_snps += 1
+            snp_chr, snp_locus, snp_pat, snp_mat = snp_file_line.strip().split()
+            snp_locus = int(snp_locus)
+            if num_snps % display_num_snps == 0:
+                sys.stderr.write("[M::" + __name__ + "] pass 3: read " + str(num_snps) + " SNPs, last at " + snp_chr + ":" + str(snp_locus) + "\n")
             
+            for pileup_column in bam_file.pileup(snp_chr, snp_locus - 1, snp_locus):
+                if pileup_column.pos == snp_locus - 1:
+                    for pileup_read in pileup_column.pileups:
+                        if not pileup_read.is_del and not pileup_read.is_refskip:
+                            # skip read if not a candidate
+                            if not seg_data.contains_read_name(pileup_read.alignment.query_name):
+                                continue
+                            # discard if low base quality
+                            if pileup_read.alignment.query_qualities[pileup_read.query_position] < min_baseq:
+                                continue
+                            
+                            # phase the corresponding segment
+                            seg_haplotype = -1
+                            if pileup_read.alignment.query_sequence[pileup_read.query_position] == snp_pat:
+                                seg_haplotype = 0
+                            elif pileup_read.alignment.query_sequence[pileup_read.query_position] == snp_mat:
+                                seg_haplotype = 1
+                            else:
+                                continue
+                            seg_data.update_haplotype(pileup_read.alignment.query_name, pileup_read.alignment.is_read2, snp_chr, snp_locus, seg_haplotype)
+    sys.stderr.write("[M::" + __name__ + "] pass 3 done: read " + str(num_snps) + " SNPs\n")
+    
+    # write output
+    sys.stderr.write("[M::" + __name__ + "] writing output for " + str(seg_data.num_reads()) + " candidate reads, containing " + str(seg_data.num_segs()) + " segments (" + str(round(100.0 * seg_data.num_phased_segs() / seg_data.num_segs(), 2)) + "% phased)\n")
+    sys.stdout.write(seg_data.to_string()+"\n")
+  
+    return 0
+    
