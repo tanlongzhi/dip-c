284e2bda7ee2bac48b4fd1cf7a3565d626bfc4d8
tanlongzhi <tanlongzhi@gmail.com>
2017-11-30 16:38:07 -0500

    con3: -H merges the two haplotypes

--- con3.py
+++ con3.py
@@ -11,8 +11,12 @@ def g3d_np_arrays_to_leg(hom_names, loci_np_array, index):
     ref_name, haplotype = hom_name_to_ref_name_haplotype(hom_name)
     return Leg(ref_name, ref_locus, haplotype)
     
-def g3d_np_arrays_to_matrix_index(hom_names, loci_np_array, index, hom_offsets, matrix_bin_size):
+def g3d_np_arrays_to_matrix_index(hom_names, loci_np_array, index, hom_offsets, matrix_bin_size, merge_haplotypes):
     hom_name = hom_names[index]
+    if merge_haplotypes:
+        # merge both haplotypes into "paternal"
+        ref_name, haplotype = hom_name_to_ref_name_haplotype(hom_name)
+        hom_name = ref_name_haplotype_to_hom_name((ref_name, Haplotypes.paternal))
     ref_locus = int(loci_np_array[index])
     return hom_offsets[hom_name] + int(round(float(ref_locus) / matrix_bin_size))
     
@@ -31,7 +35,7 @@ def g3d_data_to_con_data(g3d_data, max_distance):
         con_data.add_con(Con(g3d_np_arrays_to_leg(hom_names, loci_np_array, index_1), g3d_np_arrays_to_leg(hom_names, loci_np_array, index_2)))
     return con_data
     
-def g3d_data_to_matrix(g3d_data, max_distance, hom_offsets, matrix_bin_size, matrix_size):
+def g3d_data_to_matrix(g3d_data, max_distance, hom_offsets, matrix_bin_size, matrix_size, merge_haplotypes):
     hom_names, loci_np_array, position_np_array = g3d_data.to_np_arrays()
     sys.stderr.write("[M::" + __name__ + "] preparing genome-wide KD tree\n")
     kdtree = spatial.KDTree(position_np_array)
@@ -44,8 +48,8 @@ def g3d_data_to_matrix(g3d_data, max_distance, hom_offsets, matrix_bin_size, mat
     matrix_data = np.zeros((matrix_size, matrix_size), dtype = int)
     for index_pair in index_pairs:
         index_1, index_2 = index_pair
-        matrix_index_1 = g3d_np_arrays_to_matrix_index(hom_names, loci_np_array, index_1, hom_offsets, matrix_bin_size)
-        matrix_index_2 = g3d_np_arrays_to_matrix_index(hom_names, loci_np_array, index_2, hom_offsets, matrix_bin_size)
+        matrix_index_1 = g3d_np_arrays_to_matrix_index(hom_names, loci_np_array, index_1, hom_offsets, matrix_bin_size, merge_haplotypes)
+        matrix_index_2 = g3d_np_arrays_to_matrix_index(hom_names, loci_np_array, index_2, hom_offsets, matrix_bin_size, merge_haplotypes)
         matrix_data[matrix_index_1, matrix_index_2] += 1
         matrix_data[matrix_index_2, matrix_index_1] += 1
     return matrix_data
@@ -56,10 +60,11 @@ def con3(argv):
     matrix_mode = False
     chr_len_file_name = None
     matrix_bin_size = 1000000
+    merge_haplotypes = False
     
     # read arguments
     try:
-        opts, args = getopt.getopt(argv[1:], "d:m:b:")
+        opts, args = getopt.getopt(argv[1:], "d:m:b:H")
     except getopt.GetoptError as err:
         sys.stderr.write("[E::" + __name__ + "] unknown command\n")
         return 1
@@ -69,18 +74,21 @@ def con3(argv):
         sys.stderr.write("  -d FLOAT       max distance for generating a contact [" + str(max_distance) + "]\n")
         sys.stderr.write("  -m <chr.len>   output a matrix of binned counts based on chromosome lengths (tab-delimited: chr, len)\n")
         sys.stderr.write("  -b INT         bin size (bp) for \"-m\" (bins are centered around multiples of bin size) [" + str(matrix_bin_size) + "]\n")
+        sys.stderr.write("  -H             merge the two haplotypes for \"-m\" [" + str(matrix_bin_size) + "]\n")
         return 1
         
     num_color_schemes = 0
     for o, a in opts:
         if o == "-d":
             max_distance = float(a)
-        if o == "-m":
+        elif o == "-m":
             matrix_mode = True
             chr_len_file_name = a
-        if o == "-b":
+        elif o == "-b":
             matrix_bin_size = int(a)
-                                
+        elif o == "-H":
+            merge_haplotypes = True
+                                            
     # read 3DG file
     g3d_data = file_to_g3d_data(open(args[0], "rb"))
     g3d_data.sort_g3d_particles()
@@ -98,7 +106,7 @@ def con3(argv):
         for chr_len_file_line in chr_len_file:
             ref_name, ref_len = chr_len_file_line.strip().split("\t")
             ref_len = int(ref_len)
-            for haplotype in [Haplotypes.paternal, Haplotypes.maternal]:
+            for haplotype in ([Haplotypes.paternal] if merge_haplotypes else [Haplotypes.paternal, Haplotypes.maternal]):
                 hom_name = ref_name_haplotype_to_hom_name((ref_name, haplotype))
                 hom_bin_len = int(round(float(ref_len) / matrix_bin_size)) + 1
                 hom_lens[hom_name] = ref_len
@@ -107,7 +115,7 @@ def con3(argv):
                 matrix_size += hom_bin_len
         
         # generate matrix
-        matrix_data = g3d_data_to_matrix(g3d_data, max_distance, hom_offsets, matrix_bin_size, matrix_size)
+        matrix_data = g3d_data_to_matrix(g3d_data, max_distance, hom_offsets, matrix_bin_size, matrix_size, merge_haplotypes)
         np.savetxt(sys.stdout, matrix_data, fmt='%i', delimiter='\t')
         
     else:
