7f12b9def4d6250a7bb4fd62c82e083cbf52257b
tanlongzhi <tanlongzhi@gmail.com>
2018-08-23 14:55:02 -0400

    fixed bug in interactive visualization of imputed contacts

--- con_imputed_to_juicer_pre_short.sh
+++ con_imputed_to_juicer_pre_short.sh
@@ -3,4 +3,4 @@
 input_file="$1"
 output_file=${input_file/.con.gz/.juicer.txt.gz}
 
-gunzip -c "$input_file" | awk -F'[\t,]' -v OFS='\t' '{print 0,$1 "(" ($3==0?"pat":"mat") ")",$2,0,0,$4 "(" ($6==0?"pat":"mat") ")",$5,1}' | sort -k2,2 -k6,6 | gzip -c > "$output_file"
+gunzip -c "$input_file" | awk -F'[\t,]' -v OFS='\t' '{hom1=$1 "(" ($3==0?"pat":"mat") ")"; hom2=$4 "(" ($6==0?"pat":"mat") ")"; if(hom1<hom2){print 0,hom1,$2,0,0,hom2,$5,1;}else{print 0,hom2,$5,0,0,hom1,$2,1;}}' | sort -k2,2 -k6,6 | gzip -c > "$output_file"
