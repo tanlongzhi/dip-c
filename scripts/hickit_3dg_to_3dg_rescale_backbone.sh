ec5bfde36623b1f655b19b9f7de98ddd18345ffe
tanlongzhi <tanlongzhi@gmail.com>
2018-10-13 20:18:22 -0400

    scripts/hickit_3dg_to_3dg_rescale_backbone: fixed bug during median calculation

--- scripts/hickit_3dg_to_3dg_rescale_backbone.sh
+++ scripts/hickit_3dg_to_3dg_rescale_backbone.sh
@@ -4,7 +4,7 @@ input_file="$1"
 output_file=${input_file/.3dg/.dip-c.3dg}
 
 
-backbone_unit=$(grep -v "^#" "$input_file" | awk '{chr=$1;loc=$2;x=$3;y=$4;z=$5;if(NR>=2){if(chr==prev_chr){print sqrt((prev_x-x)^2+(prev_y-y)^2+(prev_z-z)^2)}};prev_chr=chr;prev_loc=loc;prev_x=x;prev_y=y;prev_z=z;}' | sort -n | awk '{dist[NR]=$0}END{if(NR%2){print dist[(NR+1)/2]}else{print (dist[(NR/2)]+count[(NR/2)+1])/2.0}}')
+backbone_unit=$(grep -v "^#" "$input_file" | awk '{chr=$1;loc=$2;x=$3;y=$4;z=$5;if(NR>=2){if(chr==prev_chr){print sqrt((prev_x-x)^2+(prev_y-y)^2+(prev_z-z)^2)}};prev_chr=chr;prev_loc=loc;prev_x=x;prev_y=y;prev_z=z;}' | sort -n | awk '{dist[NR]=$0}END{if(NR%2){print dist[(NR+1)/2]}else{print (dist[(NR/2)]+dist[(NR/2)+1])/2.0}}')
 
 echo "input: median backbone distance = "${backbone_unit}
 
