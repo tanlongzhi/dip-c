5f3199131d02c6151ca8d062f4d6bacbcb1547d9
tanlongzhi <tanlongzhi@gmail.com>
2018-10-14 21:33:36 -0400

    scripts: replaced wrong hickit rescaling with reading unit distance

--- scripts/hickit_3dg_to_3dg_rescale.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-input_file="$1"
-output_file=${input_file/.3dg/.dip-c.3dg}
-
-num_beads=$(grep -v "^#" "$input_file" | wc -l | tr -d ' ')
-
-grep -v "^#" "$input_file" | sed 's/a	/(pat)	/g; s/b	/(mat)	/g; s/X	/X(mat)	/g; s/Y	/Y(pat)	/g' | awk -F'\t' -v OFS='\t' -v num_beads=${num_beads} 'BEGIN {norm_factor=num_beads^(1/3)*2/10} {print $1,$2,$3*norm_factor,$4*norm_factor,$5*norm_factor}' > "$output_file"
-
---
+++ scripts/hickit_3dg_to_3dg_rescale_unit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+input_file="$1"
+output_file=${input_file/.3dg/.dip-c.3dg}
+
+
+backbone_unit=$(grep "^#unit" "$input_file" | cut -d' ' -f2)
+
+echo "input: distance unit = "${backbone_unit}
+
+grep -v "^#" "$input_file" | sed 's/a	/(pat)	/g; s/b	/(mat)	/g; s/X	/X(mat)	/g; s/Y	/Y(pat)	/g' | awk -F'\t' -v OFS='\t' -v backbone_unit=${backbone_unit} 'BEGIN {norm_factor=2.0/backbone_unit} {print $1,$2,$3*norm_factor,$4*norm_factor,$5*norm_factor}' > "$output_file"
+
