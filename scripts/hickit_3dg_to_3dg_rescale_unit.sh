a54d7e1efc01bfeaa851290360ec2953b6b44035
Longzhi Tan <tanlongzhi@gmail.com>
2019-02-05 21:50:00 -0500

    script/hickit_3dg_to_3dg_rescale_unit: fixed typo
    1 unit should equal to 1.0 particle radius, not 2.0, according to nuc_dynamics

--- scripts/hickit_3dg_to_3dg_rescale_unit.sh
+++ scripts/hickit_3dg_to_3dg_rescale_unit.sh
@@ -8,5 +8,5 @@ backbone_unit=$(grep "^#unit" "$input_file" | cut -d' ' -f2)
 
 echo "input: distance unit = "${backbone_unit}
 
-grep -v "^#" "$input_file" | sed 's/a	/(pat)	/g; s/b	/(mat)	/g; s/X	/X(mat)	/g; s/Y	/Y(pat)	/g' | awk -F'\t' -v OFS='\t' -v backbone_unit=${backbone_unit} 'BEGIN {norm_factor=2.0/backbone_unit} {print $1,$2,$3*norm_factor,$4*norm_factor,$5*norm_factor}' > "$output_file"
+grep -v "^#" "$input_file" | sed 's/a	/(pat)	/g; s/b	/(mat)	/g; s/X	/X(mat)	/g; s/Y	/Y(pat)	/g' | awk -F'\t' -v OFS='\t' -v backbone_unit=${backbone_unit} 'BEGIN {norm_factor=1.0/backbone_unit} {print $1,$2,$3*norm_factor,$4*norm_factor,$5*norm_factor}' > "$output_file"
 
