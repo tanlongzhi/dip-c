8a4126dfc0e15f5deef1b570e169ae242ef2e2d2
tanlongzhi <tanlongzhi@gmail.com>
2019-09-12 09:48:37 -0400

    scripts/hickit_pairs_to_con handles files with no phases

--- scripts/hickit_pairs_to_con.sh
+++ scripts/hickit_pairs_to_con.sh
@@ -3,4 +3,4 @@
 input_file="$1"
 output_file=${input_file/.pairs.gz/.con.gz}
 
-gunzip -c "$input_file" | grep -v "^#" | awk -v OFS='\t' '{print $2","$3","$8, $4","$5","$9}' | gzip -c > "$output_file"
+gunzip -c "$input_file" | grep -v "^#" | awk -v OFS='\t' '{if($8==""){$8="."};if($9==""){$9="."};print $2","$3","$8, $4","$5","$9}' | gzip -c > "$output_file"
